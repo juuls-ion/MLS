2fcc5f8a2ad5f0ac9bf1634e79109ea928b213e8
Ollie Jones <oathompsonjones@gmail.com>
2025-04-10 20:54:38 +0100

    Add my distance functions (untested and mostly written by copilot. Will double check and test on DICE in the morning

---
+++ task-1/distances_ollie.py
@@ -0,0 +1,72 @@
+import torch as tch
+import cupy as cp
+import triton as tr
+
+########
+# CUPY #
+########
+
+
+def distance_cosine_CUPY(X, Y):
+    return 1 - (cp.dot(X, Y.T) / (cp.linalg.norm(X, axis=1, keepdims=True) * cp.linalg.norm(Y, axis=1, keepdims=True)))
+
+
+def distance_l2_CUPY(X, Y):
+    return cp.sum(cp.square(X[:, None, :] - Y[None, :, :]), axis=-1)
+
+
+def distance_dot_CUPY(X, Y):
+    return 1 - cp.dot(X, Y.T)
+
+
+def distance_manhattan_CUPY(X, Y):
+    return cp.sum(cp.abs(X[:, None, :] - Y[None, :, :]), axis=-1)
+
+
+#########
+# TORCH #
+#########
+
+def distance_cosine_TORCH(X, Y):
+    return 1 - (tch.matmul(X, Y.T) / (tch.norm(X, dim=1, keepdim=True) * tch.norm(Y, dim=1, keepdim=True).T))
+
+
+def distance_l2_TORCH(X, Y):
+    return tch.sum(tch.square(X[:, None, :] - Y[None, :, :]), dim=-1)
+
+
+def distance_dot_TORCH(X, Y):
+    return 1 - tch.matmul(X, Y.T)
+
+
+def distance_manhattan_TORCH(X, Y):
+    return tch.sum(tch.abs(X[:, None, :] - Y[None, :, :]), dim=-1)
+
+
+##########
+# TRITON #
+##########
+def distance_cosine_TRITON(X, Y, out):
+    row, col = tr.program_id(0), tr.program_id(1)
+    x = X[row, :]
+    y = Y[col, :]
+    out[row, col] = 1 - tr.sum(x * y) / (tr.sqrt(tr.sum(x * x)) * tr.sqrt(tr.sum(y * y)))
+    return out
+
+
+def distance_l2_TRITON(X, Y, out):
+    row, col = tr.program_id(0), tr.program_id(1)
+    out[row, col] = tr.sum(tr.square(X[row, :] - Y[col, :]))
+    return out
+
+
+def distance_dot_TRITON(X, Y, out):
+    row, col = tr.program_id(0), tr.program_id(1)
+    out[row, col] = 1 - tr.sum(X[row, :] * Y[col, :])
+    return out
+
+
+def distance_manhattan_TRITON(X, Y, out):
+    row, col = tr.program_id(0), tr.program_id(1)
+    out[row, col] = tr.sum(tr.abs(X[row, :] - Y[col, :]))
+    return out
